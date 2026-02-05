8b5af506090441ce517c58bcc85feba946ce211e
roey_averbuch <roey6922@gmail.com>
2026-02-05 09:14:47 +0200

    main + connection mongodb

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+from routes import route 
+
+
+app = FastAPI()
+
+
+app.include_router(route)
+
+@app.get("/")
+def health():
+    return {"status": "I'm alive!!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app",host="localhost",port=8080,reload=True)

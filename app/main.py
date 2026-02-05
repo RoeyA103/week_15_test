8a2e1fa93f4e91816248b14011ba1cb194a547c6
roey_averbuch <roey6922@gmail.com>
2026-02-05 11:45:09 +0200

    k8s files + jpg

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
 from routes import route 
-
+import json
+from connection import get_db
 
 app = FastAPI()
 
@@ -11,6 +12,16 @@ app.include_router(route)
 @app.get("/")
 def health():
     return {"status": "I'm alive!!"}
+
+@app.get("/load_data")
+def load_data():
+    db = get_db()
+    collection = db.employees
+    file_path = './data/employee_data_advanced.json'
+    with open(file_path) as file:
+        file_data = json.load(file)
+    ins_result = collection.insert_many(file_data)
+    print(f"Data inserted to MongoDB. Documents inserted: {len(ins_result.inserted_ids)}")
 
 
 # if __name__ == "__main__":
